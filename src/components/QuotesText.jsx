681ec259c8977218fd3f2ab63bed658a8bc87d60
Deriamsuarez <deriamsuarez50@gmail.com>
2022-07-24 19:29:31 -0400

    fixing responsive

--- src/components/QuotesText.jsx
+++ src/components/QuotesText.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const QuotesText = ({objStyles, randomQuote}) => {
-    console.log(objStyles)
   return (
     <div className="textContainer">
+     <i className="fa-solid fa-quote-right quote" style={objStyles[1]}></i>
     <p style={objStyles[1]}>{randomQuote.quote}</p>
     <p style={objStyles[1]}><b>{randomQuote.author}</b></p>
 </div> 
